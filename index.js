ca3e309581adde065ddf1bd15bd3a27c08cc1b97
Juan Reyes <camiloreyes1125@gmail.com>
2024-01-15 14:45:32 -0500

    basic toString,split,join

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+let num = 123456789
+
 function practice (num) {
 
-    for(let i = 1; i <= num; i++){
-  
-      if (i % 3 === 0 || i % 5 === 0) {
-        console.log("FizzBuzz")
-      } else if(i % 3 === 0){
-        console.log("Fizz")
-      } else if (i % 5 === 0){
-        console.log("Buzz")
-      } else {
-        console.log(i)
-      }
-    }
+    let numString = num.toString().split("")
+    
+    let result = numString.sort((a, b) => b-a ).join("")
+
+    console.log(result)
+
   }
-  console.log(practice(15))
+  console.log(practice(num))
